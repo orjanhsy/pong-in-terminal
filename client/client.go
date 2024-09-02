e642de7b9653869f8b64e986976bbc8b5f36ffee
oerjan <orjanhsy@gmail.com>
2024-09-02 16:30:14 +0200

    add client side tcell screen with logic to draw and move objects

--- client/client.go
+++ client/client.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/gdamore/tcell"
 	pb "github.com/orjanhsy/pong-in-terminal/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -12,6 +15,127 @@ import (
 const adress = "localhost:50051"
 
 type GameClient struct {
+	screen tcell.Screen
+	grpcClient pb.PongServiceClient
+	playerId string
+}
+
+func NewGameClient(playerId string, grpcClient pb.PongServiceClient) *GameClient {
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("Failed to create tcell screen: %v", err)
+	}
+
+	return &GameClient{
+		screen: s,
+		grpcClient: grpcClient,
+		playerId: playerId,
+	}
+}
+
+func (gc *GameClient) Start() {
+	if err := gc.screen.Init(); err != nil {
+		log.Fatalf("Failed to init screen: %v", err)
+	}
+	
+	go gc.listenForPlayerInput()
+
+	gc.recieveGameState()
+}
+
+func (gc *GameClient) Quit() {
+	gc.screen.Fini()
+	log.Println("Quit game")
+	os.Exit(0)
+}
+
+func (gc *GameClient) listenForPlayerInput() {
+	for {
+		ev := gc.screen.PollEvent()
+
+		switch ev := ev.(type){
+		case *tcell.EventResize:
+		case *tcell.EventKey:
+			switch {  
+			case	ev.Key() == tcell.KeyUp, ev.Rune() == 'k':
+				gc.sendPaddleUpdate(pb.Direction_UP)	
+			case ev.Key() == tcell.KeyDown, ev.Rune() == 'j':
+				gc.sendPaddleUpdate(pb.Direction_DOWN)
+			case ev.Key() == tcell.KeyCtrlC, ev.Rune() == 'q':
+				gc.Quit()
+				return
+			}
+		}
+	}
+}
+
+func (gc *GameClient) sendPaddleUpdate(dir pb.Direction) {
+	req := &pb.PaddleUpdateRequest{
+		PlayerId: gc.playerId,
+		Direction: dir,
+	}
+
+	resp, err := gc.grpcClient.UpdatePaddlePosition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not update paddle position: %v", err)
+	}
+	log.Printf("Reponse of UpdatePaddlePosition: %s", resp.Status)
+}
+
+func (gc *GameClient) recieveGameState() {
+	req := &pb.GameStateRequest{PlayerId: gc.playerId}
+	stream, err := gc.grpcClient.StreamGameState(context.Background(), req) 
+	if err != nil {
+		log.Fatalf("Error while recieving stream: %v", err)
+	}
+
+	for {
+		resp, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("Error while recieving stream: %v", err)
+			break
+		}
+		gc.drawGameState(resp)
+	}
+}
+
+func (gc *GameClient) drawGameState(state *pb.GameStateResponse) {
+	gc.screen.Clear()
+
+	// menu
+	ballPos := fmt.Sprintf("Current ball-position: (%d, %d)\n", state.BallPos.X, state.BallPos.Y)
+	for i, r := range ballPos {
+		gc.screen.SetContent(i, 0, r, nil, tcell.StyleDefault)
+	}
+
+	p1Pos := fmt.Sprintf("Current p1-position: (%d, %d)\n", state.P1Pos.X, state.P1Pos.Y)
+	for i, r := range p1Pos {
+		gc.screen.SetContent(i, 1, r, nil, tcell.StyleDefault)
+	}
+
+	p2Pos := fmt.Sprintf("Current p2-position: (%d, %d)\n", state.P2Pos.X, state.P2Pos.Y)
+	for i, r := range p2Pos {
+		gc.screen.SetContent(i, 2, r, nil, tcell.StyleDefault)
+	}
+
+	// ball
+	x,y := int(state.BallPos.X), int(state.BallPos.Y)
+	gc.screen.SetContent(x, y, '*', nil, tcell.StyleDefault)
+
+	// paddles 
+	// p1
+	x, y = int(state.P1Pos.X), int(state.P1Pos.Y)
+	for i := -1; i <= 1; i++ {
+		gc.screen.SetContent(x, y+i, '|', nil, tcell.StyleDefault)
+	}
+	//p2
+	x, y = int(state.P2Pos.X), int(state.P2Pos.Y)
+	for i := -1; i <= 1; i++ {
+		gc.screen.SetContent(x, y+i, '|', nil, tcell.StyleDefault)
+	}
+
+	gc.screen.Show()
+
 }
 
 
@@ -23,42 +147,8 @@ func main() {
 	defer conn.Close()
 
 	grpcClient := pb.NewPongServiceClient(conn)
-	gameClient := GameClient{}
-
-	gameClient.updatePaddlePosition(grpcClient, "2", 2, 3)
-	gameClient.recieveGameState(grpcClient, "2")
+	client := NewGameClient("1", grpcClient)
+	defer client.Quit()
+	client.Start()
 }
 
-func (gc *GameClient) updatePaddlePosition(grpcClient pb.PongServiceClient, playerID string, x int, y int) {
-	req := &pb.PaddleUpdateRequest{
-		PlayerId: playerID,
-		PaddlePosition: &pb.Coordinate{
-			X: int32(x),
-			Y: int32(y),
-		},
-	}
-
-	resp, err := grpcClient.UpdatePaddlePosition(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Could not update paddle position: %v", err)
-	}
-	log.Printf("Reponse of UpdatePaddlePosition: %s", resp.Status)
-}
-
-func (gc *GameClient) recieveGameState(grpcClient pb.PongServiceClient, playerID string) {
-	req := &pb.GameStateRequest{PlayerId: playerID}
-	stream, err := grpcClient.StreamGameState(context.Background(), req) 
-	if err != nil {
-		log.Fatalf("Error while recieving stream: %v", err)
-	}
-
-	for {
-		resp, err := stream.Recv()
-		if err != nil {
-			log.Fatalf("Error while recieving stream: %v", err)
-			break
-		}
-		log.Printf("GameState: BallPos(%d, %d), P1Pos(%d, %d), P2Pos(%d, %d)", 
-			resp.BallPos.X, resp.BallPos.Y, resp.P1Pos.X, resp.P1Pos.Y, resp.P2Pos.X, resp.P2Pos.Y)
-	}
-}
